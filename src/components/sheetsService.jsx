0443eab037a66999d24be75faebbbb286c53247d
luandev <luannguyenn.2k2@gmail.com>
2023-12-28 22:33:04 +0700

    sheet API

---
+++ src/components/sheetsService.jsx
@@ -0,0 +1,24 @@
+// sheetsService.js
+import { google } from "googleapis"
+
+let ggSheetID = import.meta.env.VITE_GG_SHEET_ID
+let ggClientID = import.meta.env.VITE_GG_CLIENT_ID
+
+const fetchSheetData = async () => {
+  try {
+    const sheets = google.sheets({ version: "v4" })
+
+    const response = await sheets.spreadsheets.values.get({
+      auth: { ggClientID }, // Replace with your OAuth2 client
+      spreadsheetId: { ggSheetID }, // Replace with your spreadsheet ID
+      range: "Sheet1", // Replace with your specific range
+    })
+
+    return response.data.values
+  } catch (error) {
+    console.error("The API returned an error: " + error)
+    throw error // Rethrow the error for the caller to handle
+  }
+}
+
+export default fetchSheetData
---
+++ src/pages/test2.jsx
@@ -0,0 +1,35 @@
+// GGSHEET
+
+// YourComponent.js
+import React, { useState, useEffect } from "react"
+import fetchSheetData from "./sheetsService"
+
+const YourComponent = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const sheetData = await fetchSheetData()
+        setData(sheetData)
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      }
+    }
+
+    loadData()
+  }, [])
+
+  return (
+    <div>
+      <h1>Google Sheets Data</h1>
+      <ul>
+        {data.map((row, index) => (
+          <li key={index}>{row.join(", ")}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default YourComponent
