c1e69228a4de16c5c3c8d618230e56a1ce7abcdb
luandev <luannguyenn.2k2@gmail.com>
2023-12-28 18:24:25 +0700

    update GOOGLE API

---
+++ src/pages/test.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react"
+import useGooglePlacesAPI from "../components/useGoogleAPI"
+// import axios from "axios"
+
+const App = () => {
+  const { fetchPlaceDetails, loading } = useGooglePlacesAPI("coffee shop")
+
+  useEffect(() => {
+    if (!loading) {
+      fetchPlaceDetails()
+        .then((place) => {
+          // You can use Axios here if needed
+          console.log(place)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  }, [loading, fetchPlaceDetails])
+
+  return <div>{loading ? "Loading..." : "Ready to fetch place details"}</div>
+}
+
+export default App
