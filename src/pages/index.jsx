8a5c59a261234d4d6f8bb057040b5772ba9e2a81
Pham Duc Dat <phamducdat@Dat-Phams-MacBook-Pro.local>
2023-12-29 01:10:08 +0700

    push code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,9 +6,8 @@ import "../css/index.css";
 const HomePage = () => {
   const navigate = useNavigate();
   const [place, setMealState] = useState(null);
-  const { fetchPlaceDetails, loading } = useGooglePlacesAPI(
-    "hoàng thị coffee nguyễn thái bình"
-  );
+  const [query, setQuery] = useState("cơm tấm long xuyên làng đại học"); // default query
+  const { fetchPlaceDetails, loading } = useGooglePlacesAPI(query);
 
   useEffect(() => {
     if (!loading) {
@@ -23,6 +22,20 @@ const HomePage = () => {
     }
   }, [loading, fetchPlaceDetails]);
 
+  const placeNames = [
+    "cơm tấm long xuyên làng đại học",
+    "cơm tấm ngô quyền",
+    "cơm xèo khu B làng đại học",
+    "Bún Bò Huế Sông Hương làng đại học",
+    "Bún Bò Huế Vĩ Dạ làng đại học",
+  ]; // array of place names
+
+  const fetchRandomMeal = () => {
+    // Randomly select a new query from the array
+    const randomQuery =
+      placeNames[Math.floor(Math.random() * placeNames.length)];
+    setQuery(randomQuery);
+  };
   const fetchMeal = () => {
     fetchPlaceDetails();
   };
@@ -34,23 +47,36 @@ const HomePage = () => {
       <div className="row">
         <div className="columns five">
           <img
-            className="rounded-full"
+            className=" w-64 h-64 rounded-full max-w-lg mx-auto"
             src={place.photos[0].getUrl()}
             alt="Meal"
+            style={{
+              objectFit: "cover",
+            }}
           />
           <div className="py-5">
             {place.name && (
-              <p className="text-center">
+              <p className="text-left">
                 <strong>Tên quán: </strong> {place.name}
+              </p>
+            )}
+            {place.rating && (
+              <p className="text-left">
+                <strong>Rating: </strong> {place.rating}
+              </p>
+            )}
+            {place.formatted_phone_number && (
+              <p className="text-left">
+                <strong>Phone number: </strong> {place.formatted_phone_number}
               </p>
             )}
           </div>
           <div className="text-center">
             <button
-              className="bg-green-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-full"
-              onClick={() => navigate("/user")}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+              onClick={() => (window.location.href = place.url)}
             >
-              Tìm hiểu thêm!
+              GO HERE
             </button>
           </div>
         </div>
@@ -59,21 +85,19 @@ const HomePage = () => {
   };
 
   return (
-    <Page className="page container">
-      <div className="indexText ">
-        <h1 className="text-center py-2">Em ơi em em có đang đói hong? </h1>
-        <h2 className="text-center py-2">Nếu em đói thì em ăn anh đi!!!</h2>
-      </div>
-      <div className="indexButton text-center py-5">
-        <button
-          className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={fetchMeal}
-        >
-          Get a meal
+    <Page className=" ">
+      <div className="bg-pink-100 min-h-screen flex flex-col items-center  p-4 align-top">
+        <button className="bg-pink-300 hover:bg-pink-400 rounded-lg shadow-lg p-5 max-w-xs w-full ">
+          <div
+            className="flex items-center justify-center"
+            onClick={fetchRandomMeal}
+          >
+            RAMDOM HERE
+          </div>
         </button>
-      </div>
-      <div id="mealContainer" className="mealContainer">
-        {renderMeal()}
+        <div id="mealContainer" className="mealContainer">
+          {renderMeal()}
+        </div>
       </div>
     </Page>
   );
--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import GooglePlacesAPI from "../components/GoogleAPI";
-
-const App = () => {
-  const handlePlaceDetailsLoaded = (placeDetails) => {
-    // Handle the loaded place details here
-    console.log(placeDetails);
-    return placeDetails;
-  };
-
-  return (
-    <div id="app">
-      <GooglePlacesAPI
-        query="Cô Gái Bán Bánh"
-        onPlaceDetailsLoaded={handlePlaceDetailsLoaded}
-      />
-    </div>
-  );
-};
-=======
 import React, { useEffect } from "react"
 import useGooglePlacesAPI from "../components/useGoogleAPI"
 // import axios from "axios"
@@ -41,6 +20,5 @@ const App = () => {
 
   return <div>{loading ? "Loading..." : "Ready to fetch place details"}</div>
 }
->>>>>>> luan-dev
 
 export default App;
