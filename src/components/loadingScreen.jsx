e417b416a8ba1895b9c5bb42e201d88b6859fcab
luandev <luannguyenn.2k2@gmail.com>
2024-01-02 10:34:04 +0700

    done loadingScreen

--- src/components/loadingScreen.jsx
+++ src/components/loadingScreen.jsx
@@ -1,9 +1,21 @@
 import React from "react"
+import { PulseLoader } from "react-spinners"
+
+import Logo from "../assets/logo.png"
 
 export default function LoadingScreen() {
   return (
-    <div className="text-center flex items-center justify-center">
-      <p>Loading...</p>
+    <div className="bg-pink-100 h-screen flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center mb-4">
+        <img src={Logo} alt="logo" className="w-16" />
+        <h1 className="text-3xl text-purple-700 font-bold text-center">HNAG</h1>
+      </div>
+      <PulseLoader
+        color="#B95CF4"
+        speedMultiplier={0.85}
+        margin={5}
+        size={10}
+      />
     </div>
   )
 }
