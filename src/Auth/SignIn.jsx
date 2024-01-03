a2dcd78916c88199b75359b0efabe8724d47af36
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-01-03 20:33:46 +0700

    .

--- src/Auth/SignIn.jsx
+++ src/Auth/SignIn.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 export default function SignIn() {
-  //https://script.google.com/macros/s/AKfycbx73EsYXkhXVOmXpFRkSHkCcRtOyq1Exdb-UG_rDwm0qYtWV0t1wLzP6Ik7zqyevB1Skg/exec
+  // const login = import.meta.env.VITE_LOGIN_API_URL;
 
-  const login = import.meta.env.VITE_LOGIN_API_URL;
+  const login = "https://script.googleusercontent.com/macros/echo?user_content_key=LqP7Piw8KAz-PHeb0HtyLaM8UgKVR6QX_dFuENIPyaK4atN7R8dMUM4Y0Xz9Gx74-M4cRgRAwN6C1CZstp2aELoO7lx_fHJom5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnM5LYq9vae8g0DeUR5443gZplSUshL-U4tZkjWD88PzYAFWIChQt_5-wMIQjIOHxdwvpfgmGYeBTKAE-kmDuFN6klnWEG43rxNz9Jw9Md8uu&lib=Ma6RDRlDqWIa9InNV8sgF6h_poDVca3qB";
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -14,17 +14,14 @@ export default function SignIn() {
       password,
     };
 
-    const dataUser = fetch(
-      login,
-      {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userInfoData),
-      }
-    )
+    const dataUser = fetch(login, {
+      method: "GET",
+      // mode: "no-cors",
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      // body: JSON.stringify(userInfoData),
+    })
       .then((res) => {
         console.log("Data submitted successfully", res);
         alert("Data submitted successfully");
