77317109df3a7ee3d093fa21e5431449b7d20b72
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-01-03 22:26:02 +0700

    .

--- src/Auth/SignIn.jsx
+++ src/Auth/SignIn.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 
 export default function SignIn() {
-  // const login = import.meta.env.VITE_LOGIN_API_URL;
-
-  const login = "https://script.googleusercontent.com/macros/echo?user_content_key=LqP7Piw8KAz-PHeb0HtyLaM8UgKVR6QX_dFuENIPyaK4atN7R8dMUM4Y0Xz9Gx74-M4cRgRAwN6C1CZstp2aELoO7lx_fHJom5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnM5LYq9vae8g0DeUR5443gZplSUshL-U4tZkjWD88PzYAFWIChQt_5-wMIQjIOHxdwvpfgmGYeBTKAE-kmDuFN6klnWEG43rxNz9Jw9Md8uu&lib=Ma6RDRlDqWIa9InNV8sgF6h_poDVca3qB";
+  const login = import.meta.env.VITE_LOGIN_API_URL;
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -15,12 +13,12 @@ export default function SignIn() {
     };
 
     const dataUser = fetch(login, {
-      method: "GET",
-      // mode: "no-cors",
-      // headers: {
-      //   "Content-Type": "application/json",
-      // },
-      // body: JSON.stringify(userInfoData),
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfoData),
     })
       .then((res) => {
         console.log("Data submitted successfully", res);
