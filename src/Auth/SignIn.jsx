85f89526b87fe1b7793425a36d830972d6b5725d
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-01-06 02:41:01 +0700

    .

--- src/Auth/SignIn.jsx
+++ src/Auth/SignIn.jsx
@@ -19,33 +19,74 @@ export default function SignIn() {
       method: "GET",
     })
       .then((res) => {
-        console.log("Data submitted successfully", res);
-        // alert("Data submitted successfully");
+        console.log(res);
       })
       .catch((error) => {
         console.error("Error:", error);
         alert("Error submitting data");
       });
-
+    if (userInfoData.username && userInfoData.password) {
+      navigate("/");
+    } else if (!userInfoData.username) {
+      alert("Tài khoản khoản không được bỏ trống!!");
+    } else if (!userInfoData.password) {
+      alert("Mật khẩu không được bỏ trống");
+    }
     console.log(dataUser);
   };
 
+  // const validateLogin = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       `${login}?username=${username}&password=${password}`
+  //     );
+
+  //     if (response.ok) {
+  //       const isValid = await response.json();
+
+  //       if (isValid) {
+  //         alert("Login successful");
+  //         navigate("/");
+  //       } else {
+  //         alert("Invalid credentials");
+  //       }
+  //     } else {
+  //       console.error("Server error:", response.status);
+  //       alert("Đã xảy ra lỗi khi đăng nhập");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error:", error);
+  //     alert("Đã xảy ra lỗi khi đăng nhập");
+  //   }
+  // };
+
   return (
-    <div>
+    <div className="h-screen bg-gray-700 pt-16 pl-4 pr-4">
+      <div className="flex flex-col justify-center items-center mb-4">
+        <div className="text-xl font-bold pb-1 text-white">
+          Chào mừng bạn quay trở lại!
+        </div>
+        <p className="text-gray-400 font-semibold">
+          Chúng tôi rất vui khi được thấy bạn!
+        </p>
+      </div>
       <div className="form-group flex flex-col">
+        <p className="text-gray-400 font-bold pb-2">Thông tin tài khoản</p>
         <input
           type="text"
-          className="form-control mb-5"
+          className="form-control mb-5 bg-zinc-900 p-3"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Tên đăng nhập"
+          style={{ color: "white" }}
         />
         <input
           type="password"
-          className="form-control mb-5"
+          className="form-control mb-5 bg-zinc-900 p-3"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Mật khẩu"
+          style={{ color: "white" }}
         />
         <div className="flex">
           <button
@@ -53,17 +94,16 @@ export default function SignIn() {
             id="loginButton"
             onClick={() => {
               // Nếu đăng nhập thành công sẽ trả về HomePage và được quyền update món ăn
-              if (userInfoData.username && userInfoData.password) {
-                navigate("/");
-                addDataUser();
-              } else if (!userInfoData.username) {
-                alert("Tài khoản khoản không được bỏ trống!!");
-              } else if (!userInfoData.password) {
-                alert("Mật khẩu không được bỏ trống");
-              }
+              addDataUser();
             }}>
             Đăng nhập
           </button>
+          {/* <button
+            className="bg-pink-300 hover:bg-pink-400 rounded-lg shadow-lg p-5 w-1/3 h-25 font-bold text-sm"
+            id="loginButton"
+            onClick={validateLogin}>
+            Đăng nhập
+          </button> */}
           <button
             className="bg-pink-300 hover:bg-pink-400 rounded-lg shadow-lg p-5 w-1/3 h-25 font-bold text-sm"
             id="signUpButton"
