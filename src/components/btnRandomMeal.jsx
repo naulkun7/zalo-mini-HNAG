3ece0c2f560eae734cf840a2947d8e25ee5cd366
Pham Duc Dat <phamducdat@Dat-Phams-MacBook-Pro.local>
2024-01-10 12:13:06 +0700

    push final ver

--- src/components/btnRandomMeal.jsx
+++ src/components/btnRandomMeal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Logo from "../assets/logo.png";
 
 const RandomMealButton = ({ combinedData, setMeal, currentMealId }) => {
   const getRandomMeal = () => {
@@ -19,13 +20,12 @@ const RandomMealButton = ({ combinedData, setMeal, currentMealId }) => {
   return (
     <div>
       <button
-      className="bg-pink-300 hover:bg-pink-400 rounded-lg shadow-lg p-5 max-w-xs w-full font-bold text-xl"
-      onClick={getRandomMeal}
-    >
-      RANDOM MEAL
-    </button>
+        className="bg-pink-300 hover:bg-pink-400 rounded-lg shadow-lg p-5 max-w-xs w-full font-bold text-xl"
+        onClick={getRandomMeal}
+      >
+        <img src={Logo} alt="logo" className="w-16" />
+      </button>
     </div>
-    
   );
 };
 
@@ -33,7 +33,7 @@ RandomMealButton.propTypes = {
   combinedData: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
-    }),
+    })
   ),
   setMeal: PropTypes.func.isRequired,
   currentMealId: PropTypes.number,
